ee1d7dbd54e920b73fa76a5a0580e616e48848bd
ajp84 <prater.anthony.j@gmail.com>
2021-01-27 17:05:59 -0800

    created calculator file

---
+++ lesson02/calculator.js
@@ -0,0 +1,28 @@
+let a = 3;
+let b = 5;
+var c = 7;
+const q = a*b;
+const r = a*c;
+let s = b*c;
+
+for (a;b+a>=c; c++) {
+  console.log('a');
+}
+
+console.log(q*a);
+
+var goat= (a*b*c)*(q*r*s);
+
+console.log(goat);
+
+console.log(goat-2)
+
+let today = goat/a;
+
+console.log(today);
+
+let prop = b**2-q%4+(+3)-(1);
+
+console.log(prop)
+
+console.log(b**2-q%4+(+3)-(1));
