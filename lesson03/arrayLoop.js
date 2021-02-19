04b4ed181472d23a57186916c4e0de6af40ba3f2
ajp84 <prater.anthony.j@gmail.com>
2021-02-18 17:42:13 -0800

    lesson3 challenge3 script

---
+++ lesson03/arrayLoop.js
@@ -0,0 +1,5 @@
+const nba = ["Jazz", "Lakers", "Clippers", "Suns", "Trail Blazers", "Spurs", "Nuggets", "Warriors"];
+
+for (let i = 0; i < nba.length; i++) {
+    console.log([i] + "," + nba[i]);
+}
