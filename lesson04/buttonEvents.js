d80328bedeaf606a5515c735d37f16f8d3d0c866
ajp84 <prater.anthony.j@gmail.com>
2021-02-25 18:19:47 -0800

    challenge1

---
+++ lesson04/buttonEvents.js
@@ -0,0 +1,14 @@
+function numDubs() {
+  let input = prompt('Enter a number');
+  let answer = 0;
+  answer = Number(input)* Number(input);
+  alert(answer);
+}
+
+function nummulti() {
+  let first = prompt('Enter a number');
+  let second = prompt('Enter a second number');
+  let answer = 0;
+  answer = Number(first)* Number(second);
+  alert(answer);
+}
